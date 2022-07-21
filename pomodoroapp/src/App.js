c9b07dd6d907872fcb7276c1311610e919c11a7e
Anthony Ibrail Ortega Méndez <anthony.ibrail@gmail.com>
2022-07-21 15:23:08 -0500

    Agregué otras dos rutas, profile y about

--- pomodoroapp/src/App.js
+++ pomodoroapp/src/App.js
@@ -4,6 +4,8 @@ import "../node_modules/bootstrap-icons/font/bootstrap-icons.css";
 import Navbar from "./components/Navbar";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import PomodoroPage from "./components/pomodoroPage";
+import Profile from "./components/Profile";
+import About from "./components/About";
 
 /* 
 TODO: primero vamos a hacer la versión de celular.
@@ -18,6 +20,8 @@ function App() {
             <BrowserRouter>
                 <Routes>
                     <Route path="/" element={<PomodoroPage/>} />
+                    <Route path="/profile" element={<Profile/>}/>
+                    <Route path="/about" element={<About/>}/>
                 </Routes>
             </BrowserRouter>
 
